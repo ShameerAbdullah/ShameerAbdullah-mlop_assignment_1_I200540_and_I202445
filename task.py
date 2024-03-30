adb8b6c85871dab285ce7577f6af9b1ff284f433
shameerabdullah.sa7@gmail.com <shameerabdullah.sa7@gmail.com>
2024-03-30 22:01:09 +0500

    updated

--- task.py
+++ task.py
@@ -34,7 +34,8 @@ Justify preprocessing steps:
 Preprocessing
 """
 
-titanic_data.replace({'Sex': {'male': 0, 'female': 1}, 'Embarked': {'S': 0, 'C': 1, 'Q': 2}}, inplace=True)
+titanic_data.replace({'Sex': {'male': 0, 'female': 1},\
+                       'Embarked': {'S': 0, 'C': 1, 'Q': 2}}, inplace=True)
 
 
 """Duplicate the dataset"""
